421f08ab934ef7ebb279223bc303bb8d9a965e04
Batuhan Arat <arat_batuhan@windowslive.com>
2024-05-06 20:19:19 +0300

    ec2 bug fix try, max space

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://18.212.171.179:5002";
-//export const API_URL = "http://localhost:5002";
+//export const API_URL = "http://18.212.171.179:5002";
+export const API_URL = "http://localhost:5002";
