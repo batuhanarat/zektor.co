a690dfaf523f67c373acd794f240d0b66462a9e8
Batuhan Arat <arat_batuhan@windowslive.com>
2024-05-14 17:27:46 +0300

    another fix

--- client/src/PlantImageDisplay.tsx
+++ client/src/PlantImageDisplay.tsx
@@ -11,7 +11,8 @@ function PlantImageDisplay({ plant }: { plant: TPlant }) {
     // Fetch the URL for the last image ID
     const fetchImage = async () => {
       const lastImageId = plant.images[plant.images.length - 2];
-      console.log("plant images array: " + plant.images.length)
+      console.log(plant._id);
+      console.log("plant images array: " + plant.images.length);
       console.log(lastImageId);
       console.log(lastImageId);
  //a
