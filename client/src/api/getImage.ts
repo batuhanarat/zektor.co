a719e6cdd66ed38888dcb333adb699567bb39e45
Batuhan Arat <arat_batuhan@windowslive.com>
2024-05-14 17:10:07 +0300

    change

--- client/src/api/getImage.ts
+++ client/src/api/getImage.ts
@@ -6,6 +6,8 @@ import { TPlantImage } from "./getImages";
 export async function getImage(imageId:string): Promise<TPlantImage> {
 
     try {
+        console.log(imageId);
+
         const response = await fetch(`${API_URL}/plantTest/${imageId}`);
         if (!response.ok) {
             throw new Error(`Failed to fetch image: ${response.statusText}`);
