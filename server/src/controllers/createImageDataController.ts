be3f6be6a44de52dbf8799700483f2985c93eb45
Batuhan Arat <arat_batuhan@windowslive.com>
2024-05-15 14:36:47 +0300

    please

--- server/src/controllers/createImageDataController.ts
+++ server/src/controllers/createImageDataController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import * as WebSocket from 'ws';
+import WebSocket from 'ws';
 
 // Create WebSocket server
 const wss = new WebSocket.Server({ port: 5005 });
@@ -8,11 +8,11 @@ const wss = new WebSocket.Server({ port: 5005 });
 wss.on('connection', (ws: WebSocket) => {
     console.log('Client connected');
 
-    // Listen for messages from the client
     ws.on('message', (message: string) => {
         console.log('Received message:', message);
     });
 });
+
 
 export async function createImageDataController(req: Request, res: Response) {
     if (!req.file) {
@@ -22,8 +22,8 @@ export async function createImageDataController(req: Request, res: Response) {
     const imageBuffer = req.file.buffer;
 
     // Send the image data to WebSocket clients
-    wss.clients.forEach((client: any) => {
-        if (client.readyState === WebSocket.OPEN) {
+    wss.clients.forEach((client: WebSocket) => {
+        if (client.readyState === client.OPEN) {
             client.send(imageBuffer);
         }
     });
